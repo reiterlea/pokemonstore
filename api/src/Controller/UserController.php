eb88e0b8702ce020a728e56abdff98e407d5a2b9
Léa Reiter <lea.reiter@epitech.eu>
2024-07-15 11:06:10 +0200

    ADD authentication

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use App\Entity\User;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class UserController extends AbstractController
 {
@@ -17,15 +18,15 @@ class UserController extends AbstractController
     public function index(EntityManagerInterface $entityManager): JsonResponse
     {
         $usersList = $entityManager->getRepository(User::class)->findAll();
-        $users = array_map(function($user) {
+        $users = array_map(function ($user) {
             return [
-            'id' => $user->getId(),
-            'email' => $user->getEmail(),
-            'first_name' => $user->getFirstName(),
-            'last_name' => $user->getLastName(),
-            'username' => $user->getUsername(),
-            'roles' => $user->getRoles(),
-        ];
+                'id' => $user->getId(),
+                'email' => $user->getEmail(),
+                'first_name' => $user->getFirstName(),
+                'last_name' => $user->getLastName(),
+                'username' => $user->getUsername(),
+                'roles' => $user->getRoles(),
+            ];
         }, $usersList);
 
         return new JsonResponse($users, Response::HTTP_OK);
@@ -69,4 +70,38 @@ class UserController extends AbstractController
 
         return new JsonResponse(['status' => 'User created!'], Response::HTTP_CREATED);
     }
+
+    #[Route('/users/{id}', name: 'delete_user', methods: ['DELETE'])]
+    public function delete($id, EntityManagerInterface $entityManager): Response
+    {
+        $user = $entityManager->getRepository(User::class)->find($id);
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'User deleted'], Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/users/{id}', name: 'update_user', methods: ['PUT'])]
+    public function update($id, EntityManagerInterface $entityManager, Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $user = $entityManager->getRepository(User::class)->find($id);
+        $user->setEmail($data['email']);
+        $user->setFirstName($data['first_name']);
+        $user->setUsername($data['username']);
+        $user->setLastName($data['last_name']);
+        $user->setRoles($data['roles']);
+        $plainPassword = $data['password'];
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plainPassword
+        );
+        $user->setPassword($hashedPassword);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'User updated!'], Response::HTTP_OK);
+    }
+
+    
 }
